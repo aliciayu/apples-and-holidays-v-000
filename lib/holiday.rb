0b147e479369fb084b21b13925f985086ea56bcc
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-26 22:35:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.collect do |season, holiday|
+  holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
       puts "  #{holiday_name.to_s.split(pattern"_").collect {|split_name|}}"
